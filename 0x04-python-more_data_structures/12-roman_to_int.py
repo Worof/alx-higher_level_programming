2ec3875e77084636cd59e7999be66dd2d5815a45
Worof <worof.eng_5760@eng.kfs.edu.eg>
2023-12-06 14:14:03 -0800

    Technical interview preparation

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_value = 0
+    prev_value = 0
+
+    for letter in reversed(roman_string):
+        value = roman_numerals[letter]
+        if value < prev_value:
+            int_value -= value
+        else:
+            int_value += value
+        prev_value = value
+
+    return int_value
