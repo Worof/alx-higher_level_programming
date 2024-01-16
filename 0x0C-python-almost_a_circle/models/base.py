29bdea627b34a985ff694eea3e702e5badbf5661
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-01-15 16:29:59 -0800

    Added from_json_string static method to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,6 +32,12 @@ class Base:
             list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(list_dicts))
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation json_string."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
 
 # For testing purposes, the code below can be in a separate file, like 0-main.py
 # if __name__ == "__main__":
