784dfec1d697bea04d30a7964a171f90850b6597
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-02-15 01:53:55 +0200

    a script that lists all privileges of the MySQL users

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -5,4 +5,4 @@ FROM temperatures  -- Replace 'temperatures' with your actual table name
 WHERE MONTH(date_field) IN (7, 8)  -- Assuming 'date_field' is the name of the date or month column
 GROUP BY city
 ORDER BY avg_temp DESC
-LIMIT 3;
+LIMIT 3
---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Show all privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show all privileges for user_0d_2, assuming user_0d_2 exists and has been granted privileges
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
