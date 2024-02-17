67d56f9ea975dac33f0df5a8adea02df5f7c003e
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-02-17 03:40:31 +0200

    a program that prints the ASCII alphabet, in lowercase, not followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_str = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            uppercase_str += chr(ord(char) - 32)
+        else:
+            uppercase_str += char
+    print("{}".format(uppercase_str))
