39c894d46b2f0011f362b79efdc6087c0c41c637
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-11 23:48:46 +0200

    a script that prints two arguments passed to it

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  const num1 = parseInt(process.argv[2]);
+  const num2 = parseInt(process.argv[3]);
+  
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log("NaN");
+  } else {
+    console.log(add(num1, num2));
+  }
+  
