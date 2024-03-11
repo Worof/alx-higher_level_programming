11d864d6d4072d82ef17ab68632ba4da3da2d8ba
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-11 23:52:38 +0200

    a script that prints two arguments passed to it

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial(n) {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+  
+  const num = parseInt(process.argv[2]);
+  
+  if (isNaN(num)) {
+    console.log(1); // Factorial of NaN is 1
+  } else {
+    console.log(factorial(num));
+  }  
