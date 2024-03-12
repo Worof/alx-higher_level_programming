ea6153560034058605a7cec45d90521f32a43d46
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-12 07:41:38 -0700

    Resolved merge conflict in 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
-=======
-const myVar = "JavaScript is amazing";
-console.log(myVar);
->>>>>>> 1d744c702c010721fe1dd3a623328d924c06c14c
