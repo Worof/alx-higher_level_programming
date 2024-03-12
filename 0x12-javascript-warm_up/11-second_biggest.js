1d744c702c010721fe1dd3a623328d924c06c14c
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-12 02:06:41 +0200

    js is fun

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 
 function secondBiggest(arr) {
-    if (arr.length < 2) {
-      return 0;
-    }
-  
-    // Convert arguments to integers
-    const integers = arr.map(Number);
-  
-    // Sort the integers in descending order
-    const sortedIntegers = integers.sort((a, b) => b - a);
-  
-    // Return the second largest integer
-    return sortedIntegers[1];
+  if (arr.length < 2) {
+    return 0;
   }
-  
-  const args = process.argv.slice(2);
-  console.log(secondBiggest(args));
-  
+
+  // Convert arguments to integers
+  const integers = arr.map(Number);
+
+  // Sort the integers in descending order
+  const sortedIntegers = integers.sort((a, b) => b - a);
+
+  // Return the second largest integer
+  return sortedIntegers[1];
+}
+
+const args = process.argv.slice(2);
+console.log(secondBiggest(args));
