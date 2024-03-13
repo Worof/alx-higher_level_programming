440556737f0bd1ffb7501b267adcaa715df43fc4
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-12 21:08:54 -0700

    a function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+    const reversedList = [];
+    for (let i = 0; i < list.length; i++) {
+        reversedList.unshift(list[i]);
+    }
+    return reversedList;
+};
