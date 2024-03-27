ffbb82ea8cc2a0a88c6f236a17781e216c680c7f
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-27 20:09:46 +0200

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL --request GET "$1" -o/dev/null -w '%{http_code}' | grep -q "200" && cur; -sL "$1"
+# Displays the body of the response for a 200 status code response
+curl -sL "$1"
