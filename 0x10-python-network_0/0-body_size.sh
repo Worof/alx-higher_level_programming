eaf20cc66317c0c5b82abc4a398b84fe9fddcae4
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-28 16:12:14 -0700

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the body of the response
-curl -s "$1" -o /dev/null --write-out '%{size_download}\n'
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
