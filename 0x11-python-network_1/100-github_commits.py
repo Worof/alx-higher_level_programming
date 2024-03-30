8b0b5292cdfd41efc72e98b3752d3d788abbe699
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-03-29 21:19:06 -0700

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""
-Python script that lists 10 most recent commits of a repository by a specified owner.
-"""
 import requests
 import sys
 
-if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
+def list_commits(repo_name, owner_name):
+    """
+    List the 10 most recent commits of the repository and user on GitHub.
+
+    Args:
+        repo_name (str): The name of the repository.
+        owner_name (str): The owner of the repository.
+    """
     url = f"https://api.github.com/repos/Worof/alx-higher_level_programming/commits"
     response = requests.get(url)
     
     if response.status_code == 200:
         commits = response.json()
         for commit in commits[:10]:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
             print(f"{sha}: {author_name}")
     else:
-        print("Error:", response.status_code)
+        print("Failed to retrieve commits.")
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        repo_name = sys.argv[1]
+        owner_name = sys.argv[2]
+        list_commits(repo_name, owner_name)
+    else:
+        print("Usage: {} <repository name> <owner name>".format(sys.argv[0]))
